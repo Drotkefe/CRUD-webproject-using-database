c103041e756e067d00af6fca1cf53194a38aa24a
drotkefe <drotkefe@stud.uni-obuda.hu>
2022-04-24 18:22:19 +0200

    Asp.net

---
+++ VH3Q8P_SG1_21_22_2.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VH3Q8P_SG1_21_22_2.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
